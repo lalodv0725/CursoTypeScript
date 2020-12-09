92003c2bf3b5b7acc40018108cad61abc305f5b5
Lalo DV <lalodv0725@outlook.com>
2020-12-09 12:27:32 -0600

    Introduccion a las interfaces

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 "use strict";
 (() => {
-    console.log("------- Inicio PROGRAMA -------");
-    //Promesas
-    const retirarDinero = (montoRetirar) => {
-        console.log("Ejecutando retiro de dinero");
-        let dineroDisponible = 1500;
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (montoRetirar > dineroDisponible) {
-                    reject("Dinero insuficiente");
-                }
-                else {
-                    dineroDisponible = dineroDisponible - montoRetirar;
-                    resolve(dineroDisponible);
-                }
-            }, 3000);
-        });
+    //Introduccion a interfaces
+    const enviarAMision = (xmen) => {
+        console.log(`Xmen ${xmen.nombre} enviado a mision`);
     };
-    retirarDinero(7000)
-        .then((dinero) => {
-        console.log(`Dinero disponible despues de la operacion: ${dinero}`);
-    })
-        .catch((mensajeError) => {
-        console.warn(mensajeError);
-    });
-    console.log("------- Fin PROGRAMA -------");
+    const regresarAlCuartel = (xmen) => {
+        console.log(`Xmen ${xmen.nombre} regreso al cuartel`);
+    };
+    const wolverine = {
+        nombre: 'Logan',
+        edad: 60
+    };
+    enviarAMision(wolverine);
+    regresarAlCuartel(wolverine);
 })();
--- app.ts
+++ app.ts
@@ -1,35 +1,27 @@
 (() => {
 
-    console.log("------- Inicio PROGRAMA -------");
-    //Promesas
-    const retirarDinero = (montoRetirar:number):Promise<number> =>{
+    //Introduccion a interfaces
 
-        console.log("Ejecutando retiro de dinero")
-
-        let dineroDisponible: number = 1500;
-
-        return new Promise ((resolve,reject)=>{
-
-            setTimeout(() => {
-                if (montoRetirar > dineroDisponible) {
-                    reject("Dinero insuficiente");
-                } else {
-                    dineroDisponible = dineroDisponible - montoRetirar;
-                    resolve(dineroDisponible);
-                }
-            }, 3000);
-
-        });
+    interface Xmen {
+        nombre:string;
+        edad: number;
+        poder?:string;
     }
 
-    retirarDinero(7000)
-        .then((dinero:number)=> {
-            console.log(`Dinero disponible despues de la operacion: ${dinero}`);
-        })
-        .catch((mensajeError) => {
-            console.warn(mensajeError);
-        })
+    const enviarAMision = (xmen: Xmen) =>{
+        console.log(`Xmen ${xmen.nombre} enviado a mision`);
+    }
 
-    console.log("------- Fin PROGRAMA -------");
+    const regresarAlCuartel = (xmen:Xmen) =>{
+        console.log(`Xmen ${xmen.nombre} regreso al cuartel`);
+    }
+
+    const wolverine: Xmen = {
+        nombre: 'Logan',
+        edad: 60
+    }
+
+    enviarAMision(wolverine);
+    regresarAlCuartel(wolverine);
 
 })();
