0140fbbd97c538c833c3f38b54d51e5781d272d8
Lalo DV <lalodv0725@outlook.com>
2020-12-08 18:58:59 -0600

    Funciones: Parámetros opcionales, obligatorios y pr defecto

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 "use strict";
 (function () {
-    function getEdad() {
-        return 10 + 10;
+    function activar(quien, momento, objeto) {
+        if (objeto === void 0) { objeto = "batiseñal"; }
+        if (momento) {
+            console.log(quien + " encendio el/la " + objeto + " " + momento);
+        }
+        else {
+            console.log(quien + " encendio el/la " + objeto);
+        }
     }
-    var nombre = "Lalo";
-    var apellido = "Auditore";
-    var edad = 26;
-    //const salida = nombre+apellido+edad;
-    //const salida = nombre + " " + apellido + " (Edad: " + edad + ")";
-    var salida = "\n   " + nombre + " " + apellido + " \n(Edad: " + (edad + 10) + ") Usando String Template";
-    var salidaConFuncion = nombre + " " + apellido + " (Edad: " + getEdad() + ") Usando String Template y llamado de funcion";
-    console.log(salida); //Salida Esperada: Lalo Auditore (Edad: 26)
-    console.log(salidaConFuncion);
+    activar("Gordon", "noche");
 })();
--- app.ts
+++ app.ts
@@ -1,21 +1,15 @@
 (function(){
 
-    function getEdad(){
-        return 10+10;
+    function activar(quien:string, momento?:string, objeto:string = "batiseñal"){
+
+        if (momento) {
+            console.log(`${quien} encendio el/la ${objeto} ${momento}`);
+        }else{
+            console.log(`${quien} encendio el/la ${objeto}`);
+        }
+        
     }
 
-   const nombre = "Lalo";
-   const apellido = "Auditore";
-   const edad = 26;
+    activar("Gordon","noche");
 
-
-   //const salida = nombre+apellido+edad;
-   //const salida = nombre + " " + apellido + " (Edad: " + edad + ")";
-   const salida = `
-   ${nombre} ${apellido} \n(Edad: ${edad +10}) Usando String Template`;
-
-   const salidaConFuncion = `${nombre} ${apellido} (Edad: ${getEdad()}) Usando String Template y llamado de funcion`;
-
-   console.log(salida); //Salida Esperada: Lalo Auditore (Edad: 26)
-   console.log(salidaConFuncion);
 })();
