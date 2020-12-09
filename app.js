e382b6fe349f532b8c1711d48ae1113250a5f57e
Lalo DV <lalodv0725@outlook.com>
2020-12-09 15:19:31 -0600

    Tipado de funciones

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
 "use strict";
 (() => {
-    // class Avenger {
-    //     nombre:string = "Sin Nombre";
-    //     poder:string;
-    //     equipo:string;
-    //     puedePelear:boolean;
-    //     peleasGanadas:number;
-    // }
-    // class Avenger {
-    //     nombre:string;
-    //     poder:string;
-    //     equipo:string;
-    //     puedePelear?:boolean;
-    //     peleasGanadas?:number;
-    //     constructor(nombre:string, poder:string, equipo:string){
-    //         this.nombre = nombre;
-    //         this.poder = poder;
-    //         this.equipo = equipo;
-    //     }
-    // }
-    class Avenger {
-        constructor(nombre, poder, equipo, puedePelear = true, peleasGanadas) {
-            this.nombre = nombre;
-            this.poder = poder;
-            this.equipo = equipo;
-            this.puedePelear = puedePelear;
-            this.peleasGanadas = peleasGanadas;
-        }
-    }
-    const ironMan = new Avenger("IronMan", "Inteligente", "IronMan", false);
-    console.log(ironMan);
+    const sumar = (a, b) => a + b;
+    const nombre = () => "Lalo";
+    const obtenerSalario = () => {
+        return new Promise((resolve, reject) => {
+            resolve("Lalo");
+        });
+    };
+    obtenerSalario().then((exito) => console.log(exito.toUpperCase));
 })();
--- app.ts
+++ app.ts
@@ -1,46 +1,15 @@
 (() => {
 
-    // class Avenger {
+    const sumar = (a:number,b:number): number => a+b;
 
-    //     nombre:string = "Sin Nombre";
-    //     poder:string;
-    //     equipo:string;
-    //     puedePelear:boolean;
-    //     peleasGanadas:number;
+    const nombre = ():string => "Lalo";
 
-    // }
-
-    // class Avenger {
-
-    //     nombre:string;
-    //     poder:string;
-    //     equipo:string;
-    //     puedePelear?:boolean;
-    //     peleasGanadas?:number;
-
-    //     constructor(nombre:string, poder:string, equipo:string){
-    //         this.nombre = nombre;
-    //         this.poder = poder;
-    //         this.equipo = equipo;
-
-    //     }
-
-    // }
-
-    class Avenger {
-
-        constructor(public nombre:string,
-            public poder:string,
-            public equipo:string,
-            public puedePelear:boolean = true,
-            public peleasGanadas?:number){
-
-        }
+    const obtenerSalario = (): Promise<string> => {
+        return new Promise((resolve,reject) =>{
+            resolve("Lalo");
+        });
     }
 
-
-    const ironMan: Avenger = new Avenger("IronMan","Inteligente","IronMan", false);
-
-    console.log(ironMan);
+    obtenerSalario().then( (exito) => console.log(exito.toUpperCase));
     
 })();
